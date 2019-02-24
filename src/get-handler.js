052062f0a9c0702fac6e17f6cf3f765c45b22877
CaseyCurry <casey.de.curry@gmail.com>
2019-02-24 16:36:16 -0600

    handle cors

--- src/get-handler.js
+++ src/get-handler.js
@@ -5,7 +5,10 @@ export const get = async event => {
   const token = event.headers.Authorization;
   if (!token) {
     return {
-      statusCode: 401
+      statusCode: 401,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
     };
   }
   try {
@@ -22,7 +25,10 @@ export const get = async event => {
     };
   } catch (error) {
     return {
-      statusCode: 401
+      statusCode: 401,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
     };
   }
 };
